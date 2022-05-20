0c4c8c1df3e7359370ebfc69492c6fdd79ad97ef
DiegoValdez10 <88296518+DiegoValdez10@users.noreply.github.com>
2022-05-20 11:13:00 -0600

    Conexión.Java

---
+++ HDT10/Conexion.java
@@ -0,0 +1,34 @@
+/**
+ * @author Diego Valdez
+ */
+
+public class Conexion {
+    String ciudad1 = "";
+    String ciudad2 = "";
+    int km = 0;
+
+    /**
+     * constructor de conexion
+     * @param a origen
+     * @param b destino
+     * @param c km
+     */
+    public Conexion(String a, String b, String c){
+        ciudad1 = a;
+        ciudad2 = b;
+        km = Integer.parseInt(c);
+    }
+
+    /**
+     * impresiond el string
+     * @return string
+     */
+    @Override
+    public String toString() {
+        return "Conexión{" +
+                "ciudad1='" + ciudad1 + '\'' +
+                ", ciudad2='" + ciudad2 + '\'' +
+                ", km=" + km +
+                '}';
+    }
+}
