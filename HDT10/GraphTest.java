1515be7138a030eef986d3b6c25b9ae842d32e84
DiegoValdez10 <88296518+DiegoValdez10@users.noreply.github.com>
2022-05-20 16:32:05 -0600

    Pruebas Unitarias

---
+++ HDT10/GraphTest.java
@@ -0,0 +1,18 @@
+/***
+ * @author Diego Valdez
+ */
+import static org.junit.jupiter.api.Assertions.*;
+
+class GraphTest {
+    /**
+     * comprobacion del agregar
+     */
+    @org.junit.jupiter.api.Test
+    void agregar() {
+        Graph matriz = new Graph(2);
+        matriz.agregar(0,1);
+        matriz.agregar(0,1);
+        matriz.agregar(0,1);
+        assertEquals(3, matriz.devolver(0,1));
+    }
+}
